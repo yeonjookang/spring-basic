a9a974b9faff2d5ac2de26844362853b21c1cd43
euna <christinejung10@daum.net>
2023-04-24 22:12:19 +0900

    feat: HomeController 스켈레톤 코드

---
+++ src/main/java/kuit/springbasic/web/controller/HomeController.java
@@ -0,0 +1,19 @@
+package kuit.springbasic.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+public class HomeController {
+
+    @RequestMapping("/")
+    public String showHome(Model model) {
+        log.info("HomeController.homeV2");
+
+        // 코드 추가 필요
+
+        return "home";
+    }
+
+}
