b4e73368ff1cce293042eff3f4b80d347e92e6c4
kang yeon joo <rkdduswn0409@gmail.com>
2023-05-19 21:28:39 +0900

    feat : Home, LoginController

--- src/main/java/kuit/springbasic/web/controller/LoginController.java
+++ src/main/java/kuit/springbasic/web/controller/LoginController.java
@@ -5,15 +5,19 @@ import kuit.springbasic.web.dao.UserDao;
 import kuit.springbasic.web.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.SQLException;
+
+import static kuit.springbasic.config.Constant.USER_SESSION_KEY;
 
 
 @Controller
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class LoginController {
-
     private final UserDao userDao;
     @RequestMapping("/loginForm")
     public String forwardUserLoginForm() {
@@ -24,13 +28,29 @@ public class LoginController {
     public String forwardUserLoginFiled() {
         return "/user/loginFailed";
     }
-
     @RequestMapping("/login")
-    public String login(@RequestParam("userId") String userId, @RequestParam("password") String password,
-                        HttpServletRequest request){
+    public String loginV1(@RequestParam("userId") String userId, @RequestParam("password") String password,
+                        HttpServletRequest request) throws SQLException {
 
         User loggedInUser = new User(userId,password);
+        User user = userDao.findByUserId(userId);
 
+        if(user!=null&user.equals(loggedInUser)){
+            request.getSession().setAttribute(USER_SESSION_KEY,user);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
+    }
+    @RequestMapping("/login")
+    public String loginV2(@ModelAttribute User loggedInUser, HttpServletRequest request) throws SQLException {
+
+        User user = userDao.findByUserId(loggedInUser.getUserId());
+
+        if(user!=null&user.equals(loggedInUser)){
+            request.getSession().setAttribute(USER_SESSION_KEY,user);
+            return "redirect:/";
+        }
+        return "redirect:/user/loginFailed";
     }
 }
 
--- src/main/java/kuit/springbasic/web/domain/User.java
+++ src/main/java/kuit/springbasic/web/domain/User.java
@@ -35,4 +35,19 @@ public class User {
         this.email = updateUser.email;
     }
 
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
