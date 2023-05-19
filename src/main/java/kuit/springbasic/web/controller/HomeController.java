6c7d7a9f55a2f9af96a84272ef4eb202ef407886
kang yeon joo <rkdduswn0409@gmail.com>
2023-05-19 12:29:55 +0900

    commit

--- src/main/java/kuit/springbasic/web/controller/HomeController.java
+++ src/main/java/kuit/springbasic/web/controller/HomeController.java
@@ -1,13 +1,18 @@
 package kuit.springbasic.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 @Slf4j
 public class HomeController {
 
-    @RequestMapping("/")
+    //@RequestMapping(value="/", method= RequestMethod.GET)
+    @GetMapping("/")
     public String showHome(Model model) {
         log.info("HomeController.showHome");
 
